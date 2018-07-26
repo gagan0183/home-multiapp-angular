b3a90dd7e1862e45ef31be5952ae77b5eb678f86
Gagan <g.deepsingh1@gmail.com>
2018-07-27 00:44:24 +0530

    router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './course-list/course-list.component';
 import { CourseComponent } from './course/course.component';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 import { CourseService } from './services/course.service';
 
+import { routes } from './app.route';
 import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -27,6 +28,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     HttpModule,
     BrowserAnimationsModule,
     Angular2FontawesomeModule,
+    RouterModule.forRoot(routes, {useHash: true}),
     MatExpansionModule
   ],
   providers: [CourseService],
---
+++ src/app/app.route.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { CourseListComponent } from './course-list/course-list.component';
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'course-list', pathMatch: 'full'},
+    { path: 'course-list', component: CourseListComponent}
+];
