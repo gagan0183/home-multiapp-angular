0b3b183749e91dbad1dc09ae0d5e0455dcdfffb3
Gagandeep singh <g.deepsingh1@gmail.com>
2019-10-10 21:04:05 +0530

    issues fix in form

--- src/app/courses/add-course/add-course.component.ts
+++ src/app/courses/add-course/add-course.component.ts
@@ -1,17 +1,28 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'add-course',
   templateUrl: './add-course.component.html',
   styleUrls: ['./add-course.component.css']
 })
-export class AddCourseComponent {
+export class AddCourseComponent implements OnInit {
   @Input() showSideBar;
   @Output() onHideSidebar = new EventEmitter();
   categories: String[] = [];
+  formGroup: FormGroup;
 
-  constructor() {
+  constructor(private _formBuilder: FormBuilder) {
     this.categories = ['Technology', 'Java', 'Javascript', 'Python', 'GCP'];
+  }
+
+  ngOnInit() {
+    this.formGroup = this._formBuilder.group({
+      title: ['', Validators.required],
+      provider: ['', Validators.required],
+      category: ['', Validators.required],
+      description: ['', Validators.required]
+    });
   }
 
   hide() {
